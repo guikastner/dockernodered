d79b66dc1aa0cc6a685c94231fc4387a8bf047b7
KASTNER Guilherme <GKR14@dsone.3ds.com>
2024-10-07 15:45:14 -0300

    Add Dockerfile for Node-RED deployment

---
+++ docker.bat
@@ -0,0 +1,4 @@
+cd "C:\vscode\widgets\00 - Git"
+git add Dockerfile
+git commit -m "Add Dockerfile for Node-RED deployment"
+git push origin main
---
+++ dockerfile
@@ -0,0 +1,2 @@
+FROM gkastner/kastner-node-red-3dx:1.0.2
+EXPOSE 1880
